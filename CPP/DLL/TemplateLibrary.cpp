f844d069a17bd781ec18edf1ab8c9671abcbe868
crGowen <39204053+crGowen@users.noreply.github.com>
2018-05-13 03:00:18 +0100

    Sorted into folders

---
+++ CPP/DLL/TemplateLibrary.cpp
@@ -0,0 +1,17 @@
+﻿// TestLink.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "TestLink.h"
+
+namespace TemplateLibrary
+{
+	double Functions::Add(double a, double b)
+	{
+		return a + b;
+	}
+}
+
+
+
+
