e26b7553e820fb94cd6f58d6e6505e8ac6a55307
crGowen <39204053+crGowen@users.noreply.github.com>
2018-05-23 22:39:08 +0100

    Rename CPP/DLL/OpenCLtesting.cpp to CPP/OpenCL/OpenCLtesting.cpp

---
+++ CPP/OpenCL/OpenCLtesting.cpp
@@ -0,0 +1,24 @@
+﻿// OpenCLtesting.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "CL\cl.hpp"
+
+
+int main()
+{
+	std::vector<cl::Platform> all_platforms;
+	cl::Platform::get(&all_platforms);
+	if (all_platforms.size() == 0) {
+		std::cout << " No platforms found. Check OpenCL installation!\n";
+		exit(1);
+	}
+	cl::Platform default_platform = all_platforms[0];
+	std::cout << "Using platform: " << default_platform.getInfo<CL_PLATFORM_NAME>() << "\n";
+	std::cout << all_platforms.size() << " devices found.\n";
+	char c;
+	std::cin >> c;
+    return 0;
+}
+
