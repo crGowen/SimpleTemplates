940fffc97d855df4c0156842cacbc84445016882
Ryan Gowen <39204053+crGowen@users.noreply.github.com>
2019-12-17 00:31:40 +0000

    Update short.js

--- JS/short.js
+++ JS/short.js
@@ -3,10 +3,10 @@
 var $h = () => {return document.head};
 var $b = () => {return document.body};
 
-var $new = (a) => {return document.createElement(a)};
-var $new_id = (a, b) => {var c = document.createElement(a); c.id = b; return c;};
+var $new = (type) => {return document.createElement(type)};
+var $new_id = (type, name) => {var c = document.createElement(type); c.id = name; return c;};
 
-var $id = (a) => {return document.getElementById(a)};
-var $class = (a) => {return document.getElementsByClassName(a)};
+var $id = (name) => {return document.getElementById(name)};
+var $class = (clname) => {return document.getElementsByClassName(clname)};
 
-var $del = (a) => {a.parentNode.removeChild(a)};
+var $del = (elem) => {elem.parentNode.removeChild(elem)};
