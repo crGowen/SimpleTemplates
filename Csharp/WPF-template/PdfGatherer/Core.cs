af5e7a6323fbc3ab5685ad3698d4265ba95bc2e0
Ryan Gowen <39204053+crGowen@users.noreply.github.com>
2020-03-05 03:07:37 +0000

    Add files via upload

---
+++ Csharp/WPF-template/PdfGatherer/Core.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace PdfGatherer
+{
+    public class Core
+    {
+        public static bool dirAv = false;
+        public static bool fileAv = false;
+
+        public static List<String> listEntries = new List<String>();
+
+        public static void TestInit()
+        {
+            listEntries.Add("Entry template");
+            listEntries.Add("Entry template");
+            listEntries.Add("Entry template");
+            listEntries.Add("Entry template");
+            listEntries.Add("Entry template");
+            listEntries.Add("Entry template");
+            listEntries.Add("Entry template");
+            listEntries.Add("Entry template");
+            listEntries.Add("Entry template");
+            listEntries.Add("Entry template");
+            listEntries.Add("Entry template");
+        }
+    }
+
+    public class ListEntry : INotifyPropertyChanged
+    {
+        private string _text;
+
+        // a property.
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                if (_text != value)
+                {
+                    _text = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Text)));
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class EntryCollection : ObservableCollection<ListEntry>
+    {
+        public void Add(string text)
+        {
+            this.Add(new ListEntry { Text = text });
+        }
+    }
+}
