b0c83ccc56d42aa9491d461e56a2a78c6f853a8f
Ryan Gowen <39204053+crGowen@users.noreply.github.com>
2020-03-05 03:09:47 +0000

    Add files via upload

---
+++ Csharp/WPF-template/PdfGatherer/ViewModels/DirListVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfGatherer.ViewModels
+{
+    public class DirListVM
+    {
+        public static EntryCollection listItemEntries { get; } = new EntryCollection();
+    }
+}
---
+++ Csharp/WPF-template/PdfGatherer/Views/DirListPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PdfGatherer.Views
+{
+    /// <summary>
+    /// Interaction logic for DirList.xaml
+    /// </summary>
+    public partial class DirListPage : UserControl
+    {
+
+        public DirListPage()
+        {
+            InitializeComponent();
+
+            foreach (String s in Core.listEntries)
+            {
+                ViewModels.DirListVM.listItemEntries.Add(s);
+            }
+        }
+
+        private void OnLBSelectionChange(object sender, RoutedEventArgs e)
+        {
+            // stub
+        }
+
+        private void DirListCtxMenuClick(object sender, RoutedEventArgs e)
+        {
+            MenuItem itm = (MenuItem)e.Source;
+            MessageBox.Show(itm.Name);
+        }
+    }
+}
