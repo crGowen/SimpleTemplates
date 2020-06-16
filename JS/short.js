e4d78dcafdcccacef194ebfba2537269032ec1a3
Ryan Gowen <39204053+crGowen@users.noreply.github.com>
2020-06-16 17:33:46 +0100

    Update short.js

--- JS/short.js
+++ JS/short.js
@@ -1,14 +1,15 @@
 // shorthand js function declarations
+// import * as $ is intended, such that $.h will return the document head
 
-export function $h() {return document.head;};
-export function $b() {return document.body;};
+export function h() {return document.head;};
+export function b() {return document.body;};
 
-export function $new(type) {return document.createElement(type);};
-export function $new_id(type, name) {var c = document.createElement(type); c.id = name; return c;};
+export function create(type) {return document.createElement(type);};
+export function createId(type, name) {var c = document.createElement(type); c.id = name; return c;};
 
-export function $id(name) {return document.getElementById(name);};
-export function $class(clname) {return document.getElementsByClassName(clname);};
+export function byId(name) {return document.getElementById(name);};
+export function byClass(clname) {return document.getElementsByClassName(clname);};
 
-export function $del(elem) {if (elem) elem.parentNode.removeChild(elem);};
+export function del(elem) {if (elem) elem.parentNode.removeChild(elem);};
 
-export function $empty(elem) {while (elem.childNodes.length > 0) { elem.removeChild(elem.childNodes[0]);} }; 
+export function empty(elem) {while (elem.childNodes.length > 0) { elem.removeChild(elem.childNodes[0]);} }; 
