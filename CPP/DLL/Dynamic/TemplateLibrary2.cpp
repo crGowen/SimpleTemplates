d1a92fdd3f1dc3f2d1eb746cfb46c94139f1a76c
Ryan Gowen <39204053+crGowen@users.noreply.github.com>
2020-07-20 15:56:05 +0100

    Rename CPP/DLL/Explicit/TemplateLibrary2.cpp to CPP/DLL/Dynamic/TemplateLibrary2.cpp

---
+++ CPP/DLL/Dynamic/TemplateLibrary2.cpp
@@ -0,0 +1,10 @@
+﻿// TemplateLibrary2.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "TemplateLibrary2.h"
+
+double Add(double a, double b)
+	{
+		return a + b;
+	}
