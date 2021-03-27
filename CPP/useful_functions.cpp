8c68a07795b93512c451501b77286f4f3d26e3c4
Ryan Gowen <39204053+crGowen@users.noreply.github.com>
2021-03-27 14:21:17 +0000

    Update useful_functions.cpp

--- CPP/useful_functions.cpp
+++ CPP/useful_functions.cpp
@@ -1,3 +1,28 @@
+// EXCEPTION CATCHING required for these, these are simple-as-possible starting points
+
+void ReadFile(char const * const path, bool binary, std::shared_ptr<char> & bufferContent, size_t & bufferSize) {
+	auto mode = std::ios::in;
+	if (binary) mode = std::ios::binary;
+	std::ifstream iFile;
+
+	iFile.open(path, mode);
+	iFile.seekg(0, std::ios_base::end);
+	bufferSize = iFile.tellg();
+	bufferContent = std::make_shared<char>(bufferSize);
+	iFile.seekg(0, std::ios_base::beg);
+	iFile.read(bufferContent.get(), bufferSize);
+	iFile.close();
+}
+
+void WriteFile(char const* const path, bool binary, std::shared_ptr<char>& bufferContent, size_t bufferSize) {
+	auto mode = std::ios::out;
+	if (binary) mode = std::ios::binary;
+	std::ofstream oFile;
+	oFile.open(path, mode);
+	oFile.write(bufferContent.get(), bufferSize);
+	oFile.close();
+}
+
 void Split(std::vector<std::string> & result, const std::string& input, const std::string& splitter) {
 
     size_t previousPos = 0;
